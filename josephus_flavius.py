c78744f0d6221f46cf2e8a66393a29cb512baf8f
Vadym Shturkhal <vadymshturkhal@gmail.com>
2023-12-12 13:07:25 +0200

    Updated description.

--- josephus_flavius.py
+++ josephus_flavius.py
@@ -5,4 +5,9 @@
     third remaining person until no one was left. But Josephus, along with an
     unindicted co-conspirator, wanted none of this suicide nonsense; so he quickly
     calculated where he and his friend should stand in the vicious circle.
+
+    We start with N people numbered 1 to n around a circle,
+    and we eliminate every second remaining person until only one survives.
+
+    The elimination order is 2, 4, 6, 8, 10, 3, 7, 1, 9, so 5 survives.
 """
