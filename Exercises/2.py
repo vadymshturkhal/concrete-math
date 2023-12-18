3436146c131db4f8ae3825c9e1383eecd609bcbf
Vadym Shturkhal <vadymshturkhal@gmail.com>
2023-12-18 19:31:09 +0200

    Added restricted hanoi show.

--- Exercises/2.py
+++ Exercises/2.py
@@ -36,7 +36,62 @@ def hanoi_restricted_dynamic(height=1):
 
     return heights[height]
 
+from collections import deque
+
+
+def hanoi_restricted_show(discs=1, start_plank=0, finnish_plank=2):
+    if discs < 1:
+        return
+
+    circles = []
+    
+    for i in range(1, discs + 1):
+        circles.append(i)
+    
+    circles = deque(circles)
+    planks = [circles, deque(), deque()]
+
+    print(planks)
+    arrangement(len(circles), planks, start_plank, finnish_plank)
+    print(planks)
+
+def arrangement(height: int, planks, start_plank=0, finish_plank=2):
+    if height < 1:
+        return
+
+    if height == 1:
+        temp_plank = 1
+        circle = planks[start_plank].popleft()
+        planks[temp_plank].appendleft(circle)
+        print(planks)
+
+        circle = planks[temp_plank].popleft()
+        planks[finish_plank].appendleft(circle)
+        return
+
+    temp_plank = 1
+
+    # Move next two discs to temp plank
+    arrangement(height-1, planks, start_plank=start_plank, finish_plank=finish_plank)
+    print(planks)
+
+    # Move last disc to temp plank
+    second_circle = planks[start_plank].popleft()
+    planks[temp_plank].appendleft(second_circle)
+    print(planks)
+
+    arrangement(height-1, planks, start_plank=finish_plank, finish_plank=start_plank)
+    print(planks)
+
+    second_circle = planks[temp_plank].popleft()
+    planks[finish_plank].appendleft(second_circle)
+    print(planks)
+
+    # Move all discs from temp plank to the finish plank
+    arrangement(height-1, planks, start_plank=start_plank, finish_plank=finish_plank)
+
 
 h = 3
 print(hanoi_restricted(h))
 print(hanoi_restricted_dynamic(h))
+hanoi_restricted_show(h)
