8e47bd29470c5af450ee7776df3faa8b667f6f33
Vadym Shturkhal <vadymshturkhal@gmail.com>
2023-12-18 20:18:07 +0200

    Added Exercise 15 description.

---
+++ Exercises/15.py
@@ -0,0 +1,5 @@
+"""
+    Josephus had a friend who was saved by getting into the next-to-last
+    position. What is I(n), the number of the penultimate survivor when
+    every second person is executed?
+"""
