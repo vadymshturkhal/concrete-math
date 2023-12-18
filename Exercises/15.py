1024cb8d715083f5dea3e32852e1f328f5cfb776
Vadym Shturkhal <vadymshturkhal@gmail.com>
2023-12-18 20:25:17 +0200

    Implemented Exercise 15

--- Exercises/15.py
+++ Exercises/15.py
@@ -3,3 +3,33 @@
     position. What is I(n), the number of the penultimate survivor when
     every second person is executed?
 """
+
+from collections import deque
+
+
+def flavius_friend(people_number=1, *, eliminate=2):
+    if people_number < 2:
+        return 0
+
+    survivors = deque([_ for _ in range(1, people_number + 1)])
+
+    counter = 1
+    while len(survivors) > 2:
+        candidate = survivors.popleft()
+
+        if counter % eliminate != 0:
+            survivors.append(candidate)
+
+        counter += 1
+    
+    return survivors.pop()
+
+
+people_numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+
+friends = []
+for people_number in people_numbers:
+    friend =  flavius_friend(people_number)
+    friends.append(friend)
+
+print(friends)
