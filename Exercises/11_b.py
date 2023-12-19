d6df5a92dcf708d04e96605ef2cafd2bd3ac0d7c
Vadym Shturkhal <vadymshturkhal@gmail.com>
2023-12-19 20:07:29 +0200

    Added Exercise 11 b description.

---
+++ Exercises/11_b.py
@@ -0,0 +1,9 @@
+"""
+    A Double Tower of Hanoi contains 2n disks of n different sizes, two of
+    each size. As usual, we’re required to move only one disk at a time,
+    without putting a larger one over a smaller one.
+    
+    a)  What if we are required to reproduce the original top-to-bottom
+        order of all the equal-size disks in the final arrangement? [Hint:
+        This is difficult-it’s really a “bonus problem.“]
+"""
