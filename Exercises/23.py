784e18f4651175a4f5e8511dd61d32b49e83f35d
Vadym Shturkhal <vadymshturkhal@gmail.com>
2023-12-20 18:08:49 +0200

    Created description for exercise 23.

---
+++ Exercises/23.py
@@ -0,0 +1,5 @@
+"""
+    Suppose that Josephus finds himself in a given position j, but he has a
+    chance to name the elimination parameter q such that every qth person
+    is executed. Can he always save himself?
+"""
