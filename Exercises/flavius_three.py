6a126d292b38fa995c46129f8151200567c1f4a1
Vadym Shturkhal <vadymshturkhal@gmail.com>
2023-12-21 14:22:02 +0200

    Created description for flavius three.

---
+++ Exercises/flavius_three.py
@@ -0,0 +1,8 @@
+"""
+    During the Jewish-Roman war, he was among a band of 41
+    Jewish rebels trapped in a cave by the Romans. Preferring suicide to capture,
+    the rebels decided to form a circle and, proceeding around it, to kill every
+    third remaining person until no one was left. But Josephus, along with an
+    unindicted co-conspirator, wanted none of this suicide nonsense; so he quickly
+    calculated where he and his friend should stand in the vicious circle.
+"""
