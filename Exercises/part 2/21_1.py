cdcc237ea43bc116feb94079e8cbd676a37b1b73
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-01-10 19:24:03 +0200

    Changed print.

--- Exercises/part 2/21_1.py
+++ Exercises/part 2/21_1.py
@@ -1,15 +1,16 @@
-n = 162
+n = 551245
+
 sum_ = 0 
 for k in range(n + 1):
     sum_ += (-1)**(n - k) * k
-
-print(sum_)
 
 def sum_minus_one_formula(n: int):
     if n < 0:
         return 0
     
     return (1 + (-1)**n) // 2
+
+print(sum_)
 
 # Formula
 print((-sum_minus_one_formula(n) + n + 1) // 2)
