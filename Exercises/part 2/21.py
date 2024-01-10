345c46ead87689ba0de134c2185fc7d364b9e67d
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-01-10 18:25:01 +0200

    Implemented formula for ex 21.

--- Exercises/part 2/21.py
+++ Exercises/part 2/21.py
@@ -10,3 +10,12 @@ def sum_minus_one(n: int):
 
 print(sum_minus_one(1249124124))
 print(sum_minus_one(1249124121))
+
+def sum_minus_one_formula(n: int):
+    if n < 0:
+        return 0
+    
+    return (1 + (-1)**n) // 2
+
+print(sum_minus_one_formula(1249124124))
+print(sum_minus_one_formula(1249124121))
