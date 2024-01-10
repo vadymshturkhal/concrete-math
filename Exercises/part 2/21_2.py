93afb5f71f1e7e1e6052a39e62e03d6dc388f6f7
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-01-10 19:28:57 +0200

    Found and fixed annoying bug :).

--- Exercises/part 2/21_2.py
+++ Exercises/part 2/21_2.py
@@ -1,17 +1,17 @@
-def Tn(n: int):
+def Sn(n: int):
     if n < 0:
         return 0
     
     return (1 + (-1)**n) // 2
 
-def Sn(n):
-    return (-Tn(n) + n + 1) // 2
+def Tn(n):
+    return (-Sn(n) + n + 1) // 2
 
 def Un(n):
-    return (-2 * Tn(n) - Sn(n) + (n + 1)**2) / 2
+    return (-2 * Tn(n) - Sn(n) + (n + 1)**2) // 2
 
 
-n = 4345
+n = 43452
 sum_ = 0
 for k in range(n + 1):
     sum_ += (-1)**(n-k) * k**2
