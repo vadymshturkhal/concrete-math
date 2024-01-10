bcb051bf029534c1c3b741aa8099adbc12f8de41
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-01-10 19:22:44 +0200

    Changed print.

--- Exercises/part 2/21.py
+++ Exercises/part 2/21.py
@@ -8,14 +8,12 @@ def sum_minus_one(n: int):
     
     return 0
 
-print(sum_minus_one(1249124124))
-print(sum_minus_one(1249124121))
-
 def sum_minus_one_formula(n: int):
     if n < 0:
         return 0
     
     return (1 + (-1)**n) // 2
 
-print(sum_minus_one_formula(1249124124))
-print(sum_minus_one_formula(1249124121))
+n = 131216
+print(sum_minus_one(n))
+print(sum_minus_one_formula(n))
