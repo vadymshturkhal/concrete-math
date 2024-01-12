ab111b721502c67b9a03a04ad67f8a62e1c68205
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-01-12 18:38:23 +0200

    Implemented formula for ex 23 a) from part 2.

---
+++ Exercises/part 2/23_a.py
@@ -0,0 +1,18 @@
+n = 44
+sum_ = 0
+
+for k in range(1, n + 1):
+    sum_ += (2*k + 1) / (k * (k + 1))
+
+print(sum_)
+
+def harmonic(n):
+    if n <= 0:
+        return 0
+
+    return 1 / n + harmonic(n - 1)
+
+def formula(n):
+    return 2 * harmonic(n) - 1 + (1 / (n + 1))
+
+print(formula(n))
