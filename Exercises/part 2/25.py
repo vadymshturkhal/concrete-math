a10cf297d7a91ec275f03ce5b1ff7515fbe2a8dc
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-01-14 12:24:47 +0200

    Found third property of multiplication, ex 25, part 2.

--- Exercises/part 2/25.py
+++ Exercises/part 2/25.py
@@ -1,5 +1,7 @@
 """
     1) P(k from K) c * a[k] = c**(len(K)) * P(k from K) a[k]
+    2) P(k from K) a[k] * b[k] = P(k from K) a[k] * P(k from B) b[k]
+    3) P(k from K) a[k] = P(p(k) from K) a[p(k)] : p(k) - any order of set K
 """
 
 K = [1, 2, 3, 4]
@@ -35,4 +37,19 @@ for k in range(len(K)):
 
 for k in range(len(B)):
     mul_ = mul_ * B[k]
+print(mul_)
+print()
+
+# 3
+K = [1, 2, 3, 4]
+mul_ = 1
+for k in K:
+    mul_ = mul_ * k
+
+print(mul_)
+
+mul_ = 1
+for k in range(len(K) - 1, -1, -1):
+    mul_ = mul_ * K[k]
+
 print(mul_)
