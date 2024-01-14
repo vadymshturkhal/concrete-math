d0905785957e1877cef119913f32d5bf5d923e02
Vadym Shturkhal <vadymshturkhal@gmail.com>
2024-01-14 12:15:17 +0200

    Found second propertie, ex 25, part 2

--- Exercises/part 2/25.py
+++ Exercises/part 2/25.py
@@ -18,3 +18,21 @@ for k in K:
     mul_ = mul_ * k
 
 print(mul_ * c**(len(K)))
+print()
+
+# 2
+K = [1, 2, 3, 4]
+B = [5, 6, 7, 8]
+
+mul_ = 1
+for k in range(len(K)):
+    mul_ = mul_ * K[k] * B[k]
+print(mul_)
+
+mul_ = 1
+for k in range(len(K)):
+    mul_ = mul_ * K[k]
+
+for k in range(len(B)):
+    mul_ = mul_ * B[k]
+print(mul_)
